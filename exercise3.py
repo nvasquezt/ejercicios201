061dc8bb5a11009abe09b916bdb95f322227adab
Camilo Williamson <camilowillanson@gmail.com>
2022-03-12 00:06:50 -0500

    we realice a new exercise very nice to practice array

---
+++ exercise3.py
@@ -0,0 +1,17 @@
+from pickle import APPEND
+
+
+def recibe(arreglo, objetivo):
+    result=[]
+    for i in arreglo:
+        for j in arreglo:
+
+            sumaArreglo= i+j
+            if sumaArreglo == objetivo:
+                result.append(arreglo.index(i))
+                result.append(arreglo.index(j))
+    return result
+
+print(recibe([8,2,4],6))
+        
+
