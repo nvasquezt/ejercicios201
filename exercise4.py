8689addafa729e93a701f297fbd497e8318b4233
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-03-20 18:30:28 -0500

    ejercicios de  funciones

---
+++ exercise4.py
@@ -0,0 +1,19 @@
+###############################################################################
+# 1. Diseñe una funcion que calcule la cantidad de carne de aves en kilos
+# si se tienen N gallinas, M gallos y K pollitos cada uno pesando 6
+# kilos, 7 kilos y 1 kilo respectivamente.
+# 
+# 2 Mi mama me manda a comprar P panes a $ 300 cada uno, M bolsas
+# de leche a $ 3300 cada una y H huevos a $ 350 cada uno. Hacer un
+# programa que me diga las vueltas (o lo que quedo debiendo) cuando
+# me da un billete de B pesos.
+# 
+# 3 Si pido prestados P cantidad de pesos para pagarlos en dos meses, si
+# el interes del prestamo es del 3% al mes. ¿Cuanto se debe pagar al
+# final del segundo mes si el interes es compuesto mensualmente?.
+# 
+# 4 El numero de contagiados de Covid-19 en el paıs de NuncaLandia se
+# duplica cada dıa. Hacer un programa que diga el numero total de
+# personas que se han contagiado cuando pasen D dıas a partir de hoy,
+# si el numero de contagiados actuales es C.
+###############################################################################
